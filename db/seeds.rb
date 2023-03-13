2b6fac24b93d0f764ccee8f0bc803689e36284e6
eri0406 <anpanman.0607.ibu@gmail.com>
2023-03-13 11:05:42 +0900

    seedで初期データ用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# coding: utf-8
+
+Item.create!(
+    image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("public/images/book.jpg")), filename: "book.jpg"),
+    name: '本',
+    content: 'これは本です',
+    category_id: '2',
+    state_id: '2',
+    load_id: '2',
+    area_id: '2',
+    shipping_id: '2',
+    price: 1000
+)
